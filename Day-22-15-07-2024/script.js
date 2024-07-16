ae5a42f32c38f236a4d0f910fb3fababfdde2aa9
Vishal Kumar <rajesh.kumaragn519@gmail.com>
2024-07-16 20:15:38 +0530

    3rd commit

---
+++ Day-22-15-07-2024/script.js
@@ -0,0 +1,25 @@
+// console.log("Hello World!")
+
+
+
+
+let variable_name = "New Variable Data";
+console.log(variable_name);
+
+
+
+
+
+
+// Comments are non useful contents for a program but, most useful contents for a programmer
+
+let a = 5;
+// let b = 10;
+
+// Single Line Comments = //
+
+
+/* Multi-line Comments =
+This is multi-line comment */
+
+console.log("a:", a);
