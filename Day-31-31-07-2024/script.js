59d689a6648ff68112b269f9b05b5d8549a16eca
Vishal Kumar <rajesh.kumaragn519@gmail.com>
2024-08-12 20:16:31 +0530

    eighth commit

--- Day-31-31-07-2024/script.js
+++ Day-31-31-07-2024/script.js
@@ -41,4 +41,45 @@
 // }
 
 
+/* ------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
+
+/*
+// ARRAY EXAMPLE:-
+    
+    let array1 = ["Nano", "Mustang", "Bugati", "Alto", "Porsche"];  //Collection of cars
+    let array2 = [7, 6, 8, 4, 1];  //Astro Number 
+    
+    // Algorithm
+    // 1:- Traverse / Traversing / Traversal Algorithm
+    
+    // increment factor = index
+    // starting position 0
+    
+    for (let index = 0; index < array2.length; index++) {
+    
+    console.log(array2[index]);
+    }
+    
+    
+    console.log(array2[2])  */
+
+
+ 
+/* ------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+// MAPS
+
+let cars = ["Nano", "Mustang", "Bugati", "Alto", "Porsche"];  //Collection of cars
+    
+    // special function for array
+    
+    cars.map((value) => () => {
+        console.log(value);
+
+
+    // if you want the output in upparcase then,
+    
+    cars.map((value) => console.log(value.toUpperCase()));
+        });
---
+++ Day-33-02-08-2024/script.js
@@ -0,0 +1,77 @@
+/* 
+
+1. Write a JavaScript function printMultiplicationTable 
+that takes a number n as an argument and prints the 
+multiplication table for numbers from 1 to n.
+
+*/
+
+
+// function printMultiplicationTable (n) {
+//     // let n = prompt("Enter your desired number:");
+
+//     for(let times = 1; times <= 10; times++) {
+//         console.log(`${n} * ${times} = ${n * times}`);
+//     }
+// }
+
+// printMultiplicationTable(5);
+
+
+/* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+/*
+
+2. Write a JavaScript function isPalindrome that takes 
+a string as an argument and returns true if the string 
+is a palindrome (reads the same forward and backward) 
+    and false otherwise.
+
+*/
+
+
+// function isPalindrome(string) {
+
+// // let string = prompt("Please enter the word:");
+
+// let reversed = string.split(" ").reverse().join(" ");
+
+// return string === reversed;
+
+// }
+
+// console.log(isPalindrome("string"));
+
+
+/* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* 
+
+3. Create a JavaScript function factorial that takes a 
+number as an argument and returns the factorial of that 
+number. (The factorial of a non-negative integer 𝑛 is the 
+product of all positive integers less than or equal to 𝑛.)
+
+
+*/
+
+
+// function factorial(n) {
+//     // let n = prompt("Please enter a number");
+
+//     if (n < 0) {
+//         return "Their is no any factorial of negative number.";
+//     }
+
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= i;   
+//      // result = result * i
+//         }
+
+//         return result;
+// }
+
+
+// console.log(factorial(6)); 
