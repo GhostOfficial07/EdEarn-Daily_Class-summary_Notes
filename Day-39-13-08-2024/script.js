8b52c86b2b1d732458c52597c594b0f16128e4bd
Vishal0756 <0756kumarvishal@gmail.com>
2024-08-13 12:58:47 +0530

    10th commit

---
+++ Day-39-13-08-2024/script.js
@@ -0,0 +1,45 @@
+// let num = 1;
+// for(num = 1; num <=20; num++){
+//     if(num == 11){
+//         console.log("Break The Line");
+//     }
+//     console.log(num);
+// }
+
+// --------------------------------------------------------------------------------------------------------------------------//
+
+// let age = 1;
+// for(age = 1; age <= 18; age++){
+// if(age == 1){
+//     console.log("You are a baby");
+//     }else if(age <= 7){
+//         console.log("You are a kid");
+//         }else if(age <= 18){
+//             console.log("You are an adult");
+//             }else{
+//                 console.log("You are a senior");
+// }
+// }
+
+//--------------------------------------------------------------------------------------------------------------------------//
+
+// let number = 20;
+// let num = parseInt(number);
+// function multiplicationTable(){
+//     for(let i = 1; i <= 10; i++){
+//         console.log(`${num} * ${i} = ${num * i}`);
+//         }
+// }
+// multiplicationTable();
+
+//--------------------------------------------------------------------------------------------------------------------------//
+
+let Students = {
+    "Name": "VishalKumar",
+    "Age": 20,
+    "Branch": "CSE",
+    "Percentage": 85.5,
+}
+
+console.log(Students.Branch);
+
